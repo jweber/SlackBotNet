7122f74b163b021e30e429aaf233fbfa215ff637
John Weber <john@jweber.org>
2017-01-20 15:38:58 -0800

    Basic test console project

---
+++ src/SlackBotNet.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using SlackBotNet;
+using SlackBotNet.State;
+using static SlackBotNet.MatchFactory;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MainAsync().Wait();
+
+        Console.WriteLine("Press <enter> to exit");
+        Console.ReadLine();
+    }
+
+    static async Task MainAsync()
+    {
+        var bot = await SlackBot.InitializeAsync("token");
+    }
+}
