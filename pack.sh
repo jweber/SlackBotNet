a91a2b0cca98531d6ab31da605063f918c173206
John Weber <john@jweber.org>
2017-08-28 13:38:26 -0700

    Including sources and XML doc in the nuget package

--- pack.sh
+++ pack.sh
@@ -1,4 +1,4 @@
 rm -rf deploy
 
-dotnet pack src/SlackBotNet/SlackBotNet.csproj --output ../../deploy --include-symbols
-dotnet pack src/SlackBotNet.Matcher.Luis/SlackBotNet.Matcher.Luis.csproj --output ../../deploy --include-symbols
+dotnet pack src/SlackBotNet/SlackBotNet.csproj --output ../../deploy --include-symbols --include-source
+dotnet pack src/SlackBotNet.Matcher.Luis/SlackBotNet.Matcher.Luis.csproj --output ../../deploy --include-symbols --include-source
